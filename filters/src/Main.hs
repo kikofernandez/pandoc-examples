f169f1c3d7c3314af9d6bfed63a9ef7bc995926e
Kiko Fernandez Reyes <kiko.fernandez@it.uu.se>
2018-10-03 15:32:12 +0200

    almost there

--- filters/src/Main.hs
+++ filters/src/Main.hs
@@ -7,6 +7,7 @@ import Text.Pandoc
 import Text.Pandoc.Walk (walk)
 import qualified Data.Text.IO as T
 import Data.Text (Text, pack)
+import qualified Data.Map as M
 -- import Debug.Trace
 
 
@@ -17,13 +18,13 @@ writeMd p = writeMarkdown def p
 readMd :: Text -> PandocIO Pandoc
 readMd t = readMarkdown def t
 
-mdToMD :: Text -> IO ()
-mdToMD text =
-  do
-    md <- runIOorExplode $ readMd text
-    -- newMd <- walk reinterpretation md
-    result <- runIOorExplode $ writeMd md
-    T.putStrLn result
+-- mdToMD :: Text -> IO Text
+-- mdToMD text = --(runIOorExplode $ readMd) text >>= (runIOorExplode $ writeMd md)
+--   do
+--     md <- (runIOorExplode $ readMd text)
+--     -- newMd <- walk reinterpretation md
+--     runIOorExplode $ writeMd md
+
 
 removeFenceNotes :: Block -> Block
 removeFenceNotes cb@(CodeBlock (id, classes, namevals) contents) =
@@ -32,17 +33,23 @@ removeFenceNotes cb@(CodeBlock (id, classes, namevals) contents) =
        Nothing    -> cb
 removeFenceNotes x = x
 
+defaultMeta :: Meta
+defaultMeta = Meta M.empty
 
--- reinterpretation :: Block -> IO Block
--- reinterpretation cb@(CodeBlock (_, _, namevals) text) =
---   case lookup "note" namevals of
---     Just _ -> return $ Para [Str text]
---     Nothing -> return cb
--- reinterpretation x = return x
+reinterpretation :: Block -> IO [Block]
+reinterpretation cb@(CodeBlock (_, _, namevals) text) =
+  case lookup "note" namevals of
+    Just _ -> do
+      textMd <- runIOorExplode $ writeMd (Pandoc defaultMeta [Para [Str text]]) -- m Text
+      p <- runIOorExplode $ readMd textMd
+      case p of
+        Pandoc _ b -> return b
+    Nothing -> return [cb]
+reinterpretation x = return [x]
 
 main = do
   v <- catchIOError (getEnv "FILTERING_MODE") (\_ -> return "")
   case v of
-    "WITH_COMMENTS" -> T.getContents >>= mdToMD
+    "WITH_COMMENTS" -> toJSONFilter reinterpretation
     -- "NO_COMMENTS"  -> return $ toJSONFilter removeFenceNotes -- disable comments
     -- _ -> return $ return ()
