01696a1760b5b0c28825f54c18700b3548b342cb
Kiko Fernandez Reyes <kiko.fernandez@it.uu.se>
2018-10-08 23:07:33 +0200

    working header

--- zenformatting/src/Main.hs
+++ zenformatting/src/Main.hs
@@ -1,26 +1,29 @@
 module Main where
 
 import Text.Pandoc.JSON
-import System.IO.Error (catchIOError)
-import System.Environment (getEnv)
-import Text.Pandoc
-import Data.Text (Text, pack)
+-- import System.IO.Error (catchIOError)
+-- import System.Environment (getEnv)
+import Text.Pandoc ()
+-- import Data.Text (Text, pack)
 import Text.Pandoc.Walk (walk)
-import Control.Monad
-import qualified Debug.Trace as T
+-- import qualified Debug.Trace as T
 
-zenformat :: Block -> IO [Block]
-zenformat (Header x (_, classes, _) inline) = do
-  return $
-    [RawBlock (Format "html")
-              ("<p class=\"h" ++ show x
-                              ++ foldl (\acc s -> acc ++ " " ++ show s) "" classes
-                              ++ "\">"),
-     Plain inline,
-     RawBlock (Format "html") "</p>"]
-zenformat x = return $ [T.trace (show x) x]
 
+start :: Pandoc -> Pandoc
+start (Pandoc meta blocks) = Pandoc meta (walk zenformatting blocks)
+  where
+    zenformatting' (Header x (_, classes, _) inline) =
+      [RawBlock (Format "html")
+          ("<p class=\"h" ++ show x
+            ++ foldl (\acc s -> acc ++ " " ++ s) "" classes
+            ++ "\">"),
+         Plain inline,
+         RawBlock (Format "html") "</p>"
+      ]
+    zenformatting' x = return x
+
+    zenformatting [] = []
+    zenformatting (h:hs) = zenformatting' h ++ zenformatting hs
 
 main :: IO ()
-main = do
-  toJSONFilter zenformat
+main = toJSONFilter start
