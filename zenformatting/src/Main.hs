bf9162f9f4dce5d74c38229f780b8d7ddb8d2695
Kiko Fernandez Reyes <kiko.fernandez@it.uu.se>
2019-01-15 12:19:03 +0100

    add zenformatting code

--- zenformatting/src/Main.hs
+++ zenformatting/src/Main.hs
@@ -6,7 +6,10 @@ import Text.Pandoc.JSON
 import Text.Pandoc ()
 -- import Data.Text (Text, pack)
 import Text.Pandoc.Walk (walk)
--- import qualified Debug.Trace as T
+import qualified Debug.Trace as T
+import Data.Maybe (isJust, fromJust)
+
+
 
 
 start :: Pandoc -> Pandoc
@@ -20,10 +23,42 @@ start (Pandoc meta blocks) = Pandoc meta (walk zenformatting blocks)
          Plain inline,
          RawBlock (Format "html") "</p>"
       ]
+    zenformatting' HorizontalRule
+      | isJust centeredSymbol &&
+        ((isMetaInlines.fromJust) centeredSymbol) =
+          case fromJust centeredSymbol of
+            MetaInlines s -> if (length s == 1) then
+                               case s of
+                                 [Str symbol] -> return $ Para [Str symbol]
+                                 _ -> return HorizontalRule
+                             else return HorizontalRule
+            _ -> return HorizontalRule
+      | otherwise = return HorizontalRule
+    zenformatting' (Para inlines) = return $ Para $ concat (map zenformattingInline inlines)
     zenformatting' x = return x
+
+    zenformattingInline (Str s)
+      | isJust centeredSymbol &&
+        ((isMetaString.fromJust) centeredSymbol) &&
+        (s == (getMetaString.fromJust) centeredSymbol)=
+           return $ Str $ "<span class=\"centered\">" ++ s ++ "</span>"
+      | otherwise = return $ Str s
+    zenformattingInline x = return x
 
     zenformatting [] = []
     zenformatting (h:hs) = zenformatting' h ++ zenformatting hs
+
+    centeredSymbol = lookupMeta "centeredSymbol" meta
+    isMetaString (MetaString _) = True
+    isMetaString _ = False
+    isMetaInlines (MetaInlines _) = True
+    isMetaInlines _ = False
+    getMetaString (MetaString str) = str
+    getMetaString _ = "" -- should be an error
+
+
+
+
 
 main :: IO ()
 main = toJSONFilter start
